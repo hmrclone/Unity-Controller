860883eda6f17fd84911ac1a4af442877efe4b93
HM Ryan <hmryan.esq@gmail.com>
2020-09-14 02:33:49 +0100

    Instantiation tests

---
+++ MeshSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshSpawn : MonoBehaviour
+{
+    Mesh mesh;
+    public List<Vector3> verts;
+
+    public GameObject brickPrefab;
+    public float spawnTime = 1.0f;
+    //List<GameObject> bricks;
+
+    void Start()
+    {
+        mesh = this.GetComponent<MeshFilter>().sharedMesh;
+        Vector3[] vertices = mesh.vertices;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            GameObject a = Instantiate(brickPrefab) as GameObject;
+            a.transform.parent = gameObject.transform;
+            a.transform.position = vertices[i];
+            //a.GetComponent<MeshRenderer>().enabled = false;
+            //bricks.Add(a);
+        }
+        print(vertices.Length);
+    }
+
+
+    private void spawn() {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
