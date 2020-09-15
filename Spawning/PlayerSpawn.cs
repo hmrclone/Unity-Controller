554c2fd5b33544ba761dc7eed3eb6287e6843996
HM Ryan <hmryan.esq@gmail.com>
2020-09-15 15:54:56 +0100

    Cleanup and started player spawn

---
+++ Spawning/PlayerSpawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    List<GameObject> playerGhosts = new List<GameObject>();
+    public GameObject ghostPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameControl.control.playerSpawn == true) {
+            GhostSpawn();
+            GameControl.control.playerSpawn = false;
+        }
+    }
+
+    void GhostSpawn() {
+        GameObject a = Instantiate(ghostPrefab) as GameObject;
+        a.transform.parent = gameObject.transform;
+        //a.GetComponent<MeshRenderer>().enabled = false;
+        playerGhosts.Add(a);
+    }
+}
